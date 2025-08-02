bb4c94a7c47ea1f3cad4447c32857f0163275d0b
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2025-08-02 16:52:03 +0100

    move things around swaps WIP

---
+++ packages/sugar-sdk/src/primitives/externals/app/src/hooks/lib.test.ts
@@ -0,0 +1,112 @@
+import { zeroAddress } from "viem";
+import { describe, expect, test } from "vitest";
+import { initDrome } from "@/lib/test-helpers";
+import { RouteElement } from "./types.js";
+
+import {
+  
+  prepareRoute,
+  
+} from "./lib";
+
+test("Prepare route", () => {
+  const v2Nodes: RouteElement[] = [
+    {
+      from: "0x9560e827aF36c94D2Ac33a39bCE1Fe78631088Db",
+      to: "0x920Cf626a271321C151D027030D5d08aF699456b",
+      type: -1,
+      factory: "0xF1046053aa5682b4F9a81b5481394DA16BE5FF5a",
+      lp: "0xF1046053aa5682b4F9a81b5481394DA16BE5FF6b",
+      pool_fee: 0n,
+      chainId: 10,
+    },
+    {
+      from: "0x920Cf626a271321C151D027030D5d08aF699456b",
+      to: "0x4200000000000000000000000000000000000042",
+      type: -1,
+      factory: "0xF1046053aa5682b4F9a81b5481394DA16BE5FF5a",
+      lp: "0xF1046053aa5682b4F9a81b5481394DA16BE5FF6b",
+      pool_fee: 0n,
+      chainId: 10,
+    },
+  ];
+  console.log(initDrome());
+  //for a quote
+  expect(prepareRoute(initDrome(), v2Nodes, "quote")).toEqual({
+    types: ["address", "int24", "address", "int24", "address"],
+    values: [
+      "0x9560e827aF36c94D2Ac33a39bCE1Fe78631088Db",
+      4194304,
+      "0x920Cf626a271321C151D027030D5d08aF699456b",
+      4194304,
+      "0x4200000000000000000000000000000000000042",
+    ],
+  });
+  //for a quote
+  expect(
+    prepareRoute(
+      initDrome(),
+      [
+        {
+          from: "0x9560e827aF36c94D2Ac33a39bCE1Fe78631088Db",
+          to: "0x323665443CEf804A3b5206103304BD4872EA4253",
+          type: -1,
+          factory: "0xF1046053aa5682b4F9a81b5481394DA16BE5FF5a",
+          lp: "0xF1046053aa5682b4F9a81b5481394DA16BE5FF6b",
+          pool_fee: 0n,
+          chainId: 10,
+        },
+        {
+          from: "0x323665443CEf804A3b5206103304BD4872EA4253",
+          to: "0x7f5c764cbc14f9669b88837ca1490cca17c31607",
+          type: 20,
+          factory: "0xF1046053aa5682b4F9a81b5481394DA16BE5FF5a",
+          lp: "0xF1046053aa5682b4F9a81b5481394DA16BE5FF6b",
+          pool_fee: 0n,
+          chainId: 10,
+        },
+        {
+          from: "0x7f5c764cbc14f9669B88837ca1490cca17c31607",
+          to: "0x4200000000000000000000000000000000000042",
+          type: 0,
+          factory: "0xF1046053aa5682b4F9a81b5481394DA16BE5FF5a",
+          lp: "0xF1046053aa5682b4F9a81b5481394DA16BE5FF6b",
+          pool_fee: 0n,
+          chainId: 10,
+        },
+      ],
+      "quote"
+    )
+  ).toEqual({
+    values: [
+      "0x9560e827aF36c94D2Ac33a39bCE1Fe78631088Db",
+      4194304,
+      "0x323665443CEf804A3b5206103304BD4872EA4253",
+      20,
+      "0x7f5c764cbc14f9669b88837ca1490cca17c31607",
+      2097152,
+      "0x4200000000000000000000000000000000000042",
+    ],
+    types: [
+      "address",
+      "int24",
+      "address",
+      "int24",
+      "address",
+      "int24",
+      "address",
+    ],
+  });
+
+  //for a v2 swap command
+  expect(prepareRoute(initDrome(), v2Nodes, "swap")).toEqual({
+    types: ["address", "bool", "address", "bool", "address"],
+    values: [
+      "0x9560e827aF36c94D2Ac33a39bCE1Fe78631088Db",
+      false,
+      "0x920Cf626a271321C151D027030D5d08aF699456b",
+      false,
+      "0x4200000000000000000000000000000000000042",
+    ],
+  });
+});
--- packages/sugar-sdk/src/primitives/externals/app/src/hooks/lib.ts
+++ packages/sugar-sdk/src/primitives/externals/app/src/hooks/lib.ts
@@ -3,40 +3,70 @@ import { Address, encodePacked } from "viem";
 import { RouteElement } from "./types.js";
 import { DromeConfig } from "../../../../../config.js";
 
-export function prepareRoute(config: DromeConfig, nodes: RouteElement[]): {
+export const poolTypes = {
+  basic: (type: number) => type === 0 || type === -1,
+  concentrated: (type: number) => type > 50 || (type > 0 && type <= 50),
+  volatile: (type: number) => type === -1 || type > 50,
+  stable: (type: number) => type === 0 || (type > 0 && type <= 50),
+} as const;
+
+export function prepareRoute(config: DromeConfig, nodes: RouteElement[],  type: "swap" | "quote"): {
   types: string[];
   values: (Address | number)[];
 } {
+  // in each swap command, nodes are always grouped by pool type
+  const isV2Swap =
+    type === "swap" && nodes.some((n) => poolTypes.basic(n.type));
+  const types = [
+    ...[...Array(nodes.length)]
+      .map(() => ["address", "int24"])
+      .reduce((s, pool) => [...s, ...pool], []),
+    "address",
+  ];
+  const values = nodes.reduce(
+    (s, pool) => {
+      // For basic pools use specific filler values
+      let filler =
+        Number(pool.type) === 0
+          ? config.QUOTER_STABLE_POOL_FILLER
+          : config.QUOTER_VOLATILE_POOL_FILLER;
+
+      // CL pools filler is the same as their type aka tick space...
+      if (Number(pool.type) > 0) {
+        filler = Number(pool.type);
+      }
+
+      return s.length === 0
+        ? [pool.from, filler, pool.to]
+        : [...s, filler, pool.to];
+    },
+    [] as (Address | number)[]
+  );
+
+  if (isV2Swap) {
+    // replace all `int24`s with `bool`s
+    return {
+      types: types.map((t) => (t === "int24" ? "bool" : t)),
+      // replace all static filter vals with booleans
+      values: values.map((v) => {
+        if (v === config.QUOTER_STABLE_POOL_FILLER) return true;
+        if (v === config.QUOTER_VOLATILE_POOL_FILLER) return false;
+        return v;
+      }),
+    };
+  }
+
   return {
-    types: [
-      ...[...Array(nodes.length)]
-        .map(() => ["address", "int24"])
-        .reduce((s, pool) => [...s, ...pool], []),
-      "address",
-    ],
-    values: nodes.reduce(
-      (s, pool) => {
-        // For basic pools use specific filler values
-        let filler =
-          Number(pool.type) === 0
-            ? config.QUOTER_STABLE_POOL_FILLER
-            : config.QUOTER_VOLATILE_POOL_FILLER;
-
-        // CL pools filler is the same as their type aka tick space...
-        if (Number(pool.type) > 0) {
-          filler = Number(pool.type);
-        }
-
-        return s.length === 0
-          ? [pool.from, filler, pool.to]
-          : [...s, filler, pool.to];
-      },
-      [] as (Address | number)[]
-    ),
+    types,
+    values,
   };
 }
 
-export function packRoute(config: DromeConfig, nodes: RouteElement[]) {
-  const { types, values } = prepareRoute(config, nodes);
+export function packRoute(
+  config: DromeConfig,
+  nodes: RouteElement[],
+  type: "swap" | "quote" = "swap"
+) {
+  const { types, values } = prepareRoute(config, nodes, type);
   return encodePacked(types, values);
 }
