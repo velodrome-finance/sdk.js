8745ee7c1a94e5cada894e5d115a5719c2768eff
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2025-10-08 14:16:07 +0100

    add getPoolsPagination

--- packages/sugar-sdk/src/pools.test.ts
+++ packages/sugar-sdk/src/pools.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "vitest";
 
 import { init } from "@/lib/test-helpers";
 
-import { getPoolsForSwaps } from "./pools.js";
+import { getPoolsForSwaps, getPoolsPagination } from "./pools.js";
 
 describe("getPoolsForSwaps", () => {
   it("should fetch reasonable number of pools for swaps", async () => {
@@ -13,5 +13,17 @@ describe("getPoolsForSwaps", () => {
     expect(pools).toBeDefined();
     // ballpark check for number of pools
     expect(pools.length).toBeGreaterThan(1300);
+  });
+});
+
+describe("getPoolsPagination", () => {
+  it("derives pagination values from the pool count", async () => {
+    const pagination = await getPoolsPagination({
+      chainId: 10,
+      config: await init(),
+    });
+    // ballpark check for number of pools
+    expect(pagination.upperBound).toBeGreaterThan(1000);
+    expect(pagination.limit).toBeLessThanOrEqual(300);
   });
 });
--- packages/sugar-sdk/src/pools.ts
+++ packages/sugar-sdk/src/pools.ts
@@ -1,6 +1,10 @@
 import { readContract } from "@wagmi/core";
 
-import { getPoolsForSwapParams, PoolForSwap } from "./primitives/index.js";
+import {
+  getPoolsCountParams,
+  getPoolsForSwapParams,
+  PoolForSwap,
+} from "./primitives/index.js";
 import { ChainParams } from "./utils.js";
 
 export async function getPoolsForSwaps({ config, chainId }: ChainParams) {
@@ -25,4 +29,27 @@ export async function getPoolsForSwaps({ config, chainId }: ChainParams) {
     }
   }
   return result;
+}
+
+export async function getPoolsPagination({ config, chainId }: ChainParams) {
+  const poolCount = Number(
+    (await readContract(
+      config,
+      getPoolsCountParams({
+        config: config.sugarConfig,
+        chainId,
+      })
+    )) + 10n
+  ); // pad by 10 to avoid just in case there is a new pool in town during the session
+  return {
+    limit: Math.max(
+      10, // for chains with few pools, we never fetch less than 10 pools
+      Math.min(
+        // ideally we want to load the batch call with ~90 calls, giving the best performance while staying below the 100 calls cap
+        Math.floor(poolCount / 90),
+        300 // but never more than 300 pools per call to avoid "out of gas"
+      )
+    ),
+    upperBound: poolCount,
+  };
 }
