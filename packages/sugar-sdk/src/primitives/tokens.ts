96dca86a0b57c3c074ebbe18be8110d13679c8e4
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2025-10-10 14:10:14 +0100

    fix token lookup for subsets of chains

--- packages/sugar-sdk/src/primitives/tokens.ts
+++ packages/sugar-sdk/src/primitives/tokens.ts
@@ -62,8 +62,11 @@ export function getTokenPricesParams<ChainId extends number>({
 }
 
 export function getCustomPricesVars(config: Config) {
-  return Object.values(config.PRICE_MAPS).map(
-    ({ chainId, substituteToken }) => {
+  const configuredChainIds = config.chains.map((c) => c.CHAIN.id);
+
+  return Object.values(config.PRICE_MAPS)
+    .filter(({ chainId }) => configuredChainIds.includes(chainId))
+    .map(({ chainId, substituteToken }) => {
       const chainConfig = getChainConfig(config, chainId);
 
       //including 2 more tokens as USD pricing requires both ETH and stablecoin prices in the tokenlist
@@ -83,8 +86,7 @@ export function getCustomPricesVars(config: Config) {
       ];
 
       return { chainId, tokens };
-    }
-  );
+    });
 }
 
 export function getTokenPricesVars(
