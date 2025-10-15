1a306b313f7bee21d780a0ca3db188db311e0887
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2025-10-15 15:10:00 +0100

    use Math.round for slippage

--- packages/sugar-sdk/src/swap.ts
+++ packages/sugar-sdk/src/swap.ts
@@ -86,7 +86,7 @@ export async function getCallDataForSwap({
   const { planner } = getSwapVars(
     config.sugarConfig,
     quote,
-    `${Math.ceil(slippage * 100)}`,
+    `${Math.round(slippage * 100)}`,
     account
   );
 
